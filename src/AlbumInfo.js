38060ab1fdfa6d0ef44e1b551752431ecdeea3a7
Rodrigo Frazão Maia <rodrigo.frazao@armateus.com.br>
2019-05-03 17:13:32 -0300

    test(AlbumInfo):  implementado retorno de data para encadeamento

--- src/AlbumInfo.js
+++ src/AlbumInfo.js
@@ -1,9 +1,16 @@
+function createMarkup(data) {
+  return (`
+    <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
+    <p class="album-title">${data.name}</p>
+    <p class="album-artist">${data.artists[0].name}</p>
+    <p class="album-counter">${data.tracks.total} Músicas</p>
+  `);
+}
+
+
 export default function renderAlbumInfo(data, element) {
-  const markup = `
-      <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
-      <p class="album-title">${data.name}</p>
-      <p class="album-artist">${data.artists[0].name}</p>
-      <p class="album-counter">${data.tracks.total} Músicas</p>`;
-  // const markup = createMarkup(data);
+  const markup = createMarkup(data);
   element.innerHTML = markup;
+
+  return data;
 };
